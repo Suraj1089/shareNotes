6c26233d20060cd1789afaf438050b5d30242886
Suraj Pisal <surajpisal113@gmail.com>
2023-05-14 22:39:28 +0530

    changes

--- auth/config.py
+++ auth/config.py
@@ -1,8 +1,4 @@
-import os 
-
-
-MONGO_DATABASE_URI = "mongodb+srv://suraj:suraj@cluster0.fswur.mongodb.net/?retryWrites=true&w=majority"
-
+# secret keys
 SECRET_KEY = "7ed9af7637a82337a725d23f4fff51af2cfb6ff178304d1c55614b24df4c9926"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
--- auth/models.py
+++ auth/models.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Boolean, Column, Integer, String
 from . import hashing
-
 from .database import Base
 
 
 class User(Base):
+    """
+        User class for authentcation
+    """
     __tablename__ = "users"
 
     id = Column(String, primary_key=True, index=True,default=hashing.Hash.get_random_id())
