658569e4bae12ed9e234a4f28ba666df6eca7db1
Suraj Pisal <surajpisal113@gmail.com>
2023-05-30 19:16:08 +0530

    added environment variables

--- auth/app/api/config.py
+++ auth/app/api/config.py
@@ -1,9 +1,5 @@
 # secret keys
 import os 
-# SECRET_KEY = "7ed9af7637a82337a725d23f4fff51af2cfb6ff178304d1c55614b24df4c9926"
-
-# ALGORITHM = "HS256"
-# ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
