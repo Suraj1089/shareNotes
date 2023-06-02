3118df9c0e85e678433af27ac585977d70969c74
Suraj Pisal <surajpisal113@gmail.com>
2023-06-02 11:49:31 +0530

    added sqlalchmey url

--- auth/app/api/database.py
+++ auth/app/api/database.py
@@ -3,16 +3,13 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-# SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URI_AUTH')
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URI_AUTH')
 # if SQLALCHEMY_DATABASE_URL is None:
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./test.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./test.db'
 
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
-    # {
-    #     "check_some_thread":False
-    # }
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
