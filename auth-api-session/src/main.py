637bc4b9134bcfa5f91bec59a24416ecbc5b1928
Suraj Pisal <surajpisal113@gmail.com>
2023-06-05 14:49:33 +0530

    added custom classes for oauth form

--- auth-api-session/src/main.py
+++ auth-api-session/src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException, status,Depends
 from datetime import datetime, timedelta
+
+from fastapi.param_functions import Form
 from .api.schemas import User
 from .api.database import database, users  # Update with the correct import statements
 import secrets
 from .api import hashing
-from typing import Annotated
+from typing import Annotated, Dict, Optional
 from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
 app = FastAPI()
 
@@ -25,10 +27,21 @@ async def shutdown():
     await database.disconnect()
 
 
+class CustomOAuth2PasswordRequestForm(OAuth2PasswordRequestForm):
+    def __init__(self, api_key: str | None = None):
+        super().__init__(grant_type="password",  username=api_key, password="")
+
+class customOAuth2PasswordBearer(OAuth2PasswordBearer):
+    def __init__(self, tokenUrl: str):
+        super().__init__(tokenUrl)
+
 async def get_user(email: str):
     query = users.select().where(users.c.email == email) # check user already exist with email
     return await database.fetch_one(query)
 
+async def get_all_users():
+    query = users.select()
+    return await database.fetch_all(query)
 
 async def create_user(user: dict):
     api_key = secrets.token_hex(10)     # generate 10 digit token
@@ -36,10 +49,14 @@ async def create_user(user: dict):
         username=user['username'],
         email=user['email'],
         expiry_date=datetime.now() + timedelta(days=365),
-        api_key=hashing.get_password_hash(api_key)
+        api_key=api_key
     )
     user_ = await database.execute(query)
     return api_key
+
+async def authenticate(api_key: str):
+    query = users.select().where(users.c.api_key == api_key)
+    return await database.fetch_one(query)
 
 
 @app.post('/register', status_code=status.HTTP_201_CREATED)
@@ -55,7 +72,7 @@ async def register(user: User):
     return await create_user(user.dict())
 
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = customOAuth2PasswordBearer(tokenUrl="token")
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
@@ -64,46 +81,24 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     print('user')
-    # try:
-    #     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    #     username: str = payload.get("sub")
-    #     if username is None:
-    #         raise credentials_exception
-    #     token_data = TokenData(username=username)
-    # except JWTError:
-    #     raise credentials_exception
-    # user = get_user(fake_users_db, username=token_data.username)
-    # if user is None:
-    #     raise credentials_exception
-    # return user
+    return credentials_exception
 
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)]
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
 
 
 @app.post("/token")
-async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+async def login(form_data: Annotated[CustomOAuth2PasswordRequestForm, Depends()]):
+    print(form_data.username)
+    user = await authenticate("f44a025c991a2d0df905")
+    users = await get_all_users()
+    print("user in login",user)
     return {
-        "user":"login"
+        "user":users
     }
-    # user_dict = fake_users_db.get(form_data.username)
-    # if not user_dict:
-    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
-    # user = UserInDB(**user_dict)
-    # hashed_password = fake_hash_password(form_data.password)
-    # if not hashed_password == user.hashed_password:
-    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
-
-    # return {"access_token": user.username, "token_type": "bearer"}
 
 
 
 @app.get("getUserData", response_model=User)
 async def read_users_me(
-    current_user: Annotated[User, Depends(get_current_active_user)]
+    current_user: Annotated[User, Depends(get_current_user)]
 ):
     return current_user
