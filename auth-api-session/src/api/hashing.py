063cb6a4022e5d1fd0b0e0875af6253b270d63c1
Suraj Pisal <surajpisal113@gmail.com>
2023-06-05 13:08:02 +0530

    added password request form

--- auth-api-session/src/api/hashing.py
+++ auth-api-session/src/api/hashing.py
@@ -11,3 +11,5 @@ def get_password_hash(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
+
+
--- auth-api-session/src/main.py
+++ auth-api-session/src/main.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, HTTPException, status,Depends
 from datetime import datetime, timedelta
 from .api.schemas import User
 from .api.database import database, users  # Update with the correct import statements
 import secrets
 from .api import hashing
-
+from typing import Annotated
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
 app = FastAPI()
 
 
@@ -30,12 +31,7 @@ async def get_user(email: str):
 
 
 async def create_user(user: dict):
-    print('inside create_user')
-    print(user)
-    user_copy = user.copy()
     api_key = secrets.token_hex(10)     # generate 10 digit token
-    user_copy.update({"api_key": api_key})
-    
     query = users.insert().values(
         username=user['username'],
         email=user['email'],
@@ -43,9 +39,7 @@ async def create_user(user: dict):
         api_key=hashing.get_password_hash(api_key)
     )
     user_ = await database.execute(query)
-    # add user id to response
-    user_copy.update({"id":user_})
-    return user_copy
+    return api_key
 
 
 @app.post('/register', status_code=status.HTTP_201_CREATED)
@@ -53,7 +47,6 @@ async def register(user: User):
     user_ = await get_user(user.email)
     print('users are', user_)
     if user_ is not None:
-        print('error')
         raise HTTPException(
             status_code=status.HTTP_409_CONFLICT,
             detail=f'User with email {user.email} already exists!'
@@ -62,10 +55,55 @@ async def register(user: User):
     return await create_user(user.dict())
 
 
-@app.post('/login')
-def register():
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    print('user')
+    # try:
+    #     payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    #     username: str = payload.get("sub")
+    #     if username is None:
+    #         raise credentials_exception
+    #     token_data = TokenData(username=username)
+    # except JWTError:
+    #     raise credentials_exception
+    # user = get_user(fake_users_db, username=token_data.username)
+    # if user is None:
+    #     raise credentials_exception
+    # return user
+
+async def get_current_active_user(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     return {
-        "data":"login"
+        "user":"login"
     }
+    # user_dict = fake_users_db.get(form_data.username)
+    # if not user_dict:
+    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+    # user = UserInDB(**user_dict)
+    # hashed_password = fake_hash_password(form_data.password)
+    # if not hashed_password == user.hashed_password:
+    #     raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    # return {"access_token": user.username, "token_type": "bearer"}
 
 
+
+@app.get("getUserData", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
