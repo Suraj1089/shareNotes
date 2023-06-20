d9fb44e42325b00d9325edeacdc42bdbaeb0c1ad
Suraj Pisal <surajpisal113@gmail.com>
2023-06-20 09:16:55 +0530

    add salt to hashing

--- auth/app/api/routes.py
+++ auth/app/api/routes.py
@@ -4,6 +4,7 @@ from app.api.database import get_db
 from sqlalchemy.orm import Session
 from fastapi import APIRouter
 from .token import get_user,create_access_token,get_current_user
+from .config import SALT
 
 
 auth = APIRouter(
@@ -21,6 +22,7 @@ def create_user(user: schemas.UserCreate,db: Session = Depends(get_db)):
     print(user_in_db.first())
     if user_in_db.first():
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,detail=f"User with {user.email} already exist")
+
     hashed_password = hashing.Hash.bcrypt(user.password)
     new_user = models.User(email=user.email,name=user.name,hashed_password=hashed_password)
     db.add(new_user)
